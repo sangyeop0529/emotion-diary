2896ca3475d52708d7fb908fb81dd71f93520b84
DELL <sangyeop0529@gmail.com>
2023-02-07 19:08:44 +0900

    home 추가

--- src/App.js
+++ src/App.js
@@ -34,12 +34,20 @@ const reducer = (state, action) => {
 };
 
 // CONTEXTS
-
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+// DUMMY DATA
+const dummyData = [
+  { id: 1, emotion: 1, content: "오늘의 일기 1번", date: 1675749663777 },
+  { id: 2, emotion: 2, content: "오늘의 일기 2번", date: 1675749663778 },
+  { id: 3, emotion: 3, content: "오늘의 일기 3번", date: 1675749663779 },
+  { id: 4, emotion: 4, content: "오늘의 일기 4번", date: 1675749663780 },
+  { id: 5, emotion: 5, content: "오늘의 일기 5번", date: 1675749663781 },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
 
