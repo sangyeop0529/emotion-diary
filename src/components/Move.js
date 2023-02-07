9d51547c978cb6bfd83b1e84095defaa78d046a8
DELL <sangyeop0529@gmail.com>
2023-02-07 13:32:52 +0900

    Router

---
+++ src/components/Move.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Move = () => {
+  return (
+    <>
+      <Link to={"/"}>HOME</Link>
+      <br />
+      <Link to={"/new"}>NEW</Link>
+      <br />
+      <Link to={"/edit"}>EDIT</Link>
+      <br />
+      <Link to={"/diary"}>DIARY</Link>
+    </>
+  );
+};
+
+export default Move;
